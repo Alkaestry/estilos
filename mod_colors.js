102793ec26bb73cd2ea3ab1bae45d10c113774fb
Alkaestry <67449015+Alkaestry@users.noreply.github.com>
2020-06-26 15:28:39 -0600

    Create mod_colors.js

---
+++ mod_colors.js
@@ -0,0 +1,11 @@
+// Modificar los colores de la interfaz //
+/*
+  version 1.0
+*/
+
+// agregamos el script del color picker:
+var scriptCP=document.createElement('script');
+scriptCP.src='https://cdn.jsdelivr.net/gh/Alkaestry/estilos@latest/color.js';
+var head=document.getElementsByTagName('head')[0];
+head.appendChild(scriptCP);
+
